570cc9b95efb691f6c6a0d135c59d55f913aef21
josediaz30 <jose.diaz30@upr.edu>
2018-02-25 11:28:23 -0600

    Fixing dependencies

--- src/test/kotlin/io/github/jdiaz/yelp/YelpSpek.kt
+++ src/test/kotlin/io/github/jdiaz/yelp/YelpSpek.kt
@@ -5,6 +5,7 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.given
+import org.amshove.kluent.*
 
 data class ApiResult(val total: Int, val businesses: List<Map<String, String>>, val region: Map<String, String>)
 
@@ -13,10 +14,10 @@ object YelpSpek : Spek({
     val yelp = Yelp(System.getProperty("yelpkey"), null)
 
     describe("yelp client") {
-        val badYelp = Yelp("GAERgrgesrgt34t", null)
+        val badYelp = Yelp("XXX", null)
         given("an invalid API key") {
             it("should throw when attempting to fetch data") {
-                val future = badYelp.search(mapOf("term" to "food")).onFailure { false }.handle { t, u -> t }
+                { badYelp.search(mapOf("term" to "food")).get() } shouldThrow AnyException
             }
         }
 
@@ -31,6 +32,7 @@ object YelpSpek : Spek({
         val params = mapOf("term" to "food", "latitude" to "30.307182", "longitude" to "-97.755996")
         given("params $params") {
             it("should return json data describing restaurants in Austin, TX") {
+                 "hello" shouldEqual "hello"
             }
         }
     }
